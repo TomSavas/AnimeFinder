8abdf1640cdc80c9fb1c4e50ece371aa07d20aa1
Savas <tomas1savickas@gmail.com>
2016-09-29 14:54:11 +0300

    Delete refactored files

--- KissanimeEpisodeCounter.py
+++
@@ -1,8 +0,0 @@
-from HtmlParsing import *
-
-animeInfo, totalEpisodeCount, totalWatchedEpisodeCount = getAnimeInfo()
-for i in animeInfo:
-	print(i)
-print('Total episode count:', totalEpisodeCount)
-print('Total Watched episode count:', totalWatchedEpisodeCount)
-print('Which @25mins an episode averages to', int(totalWatchedEpisodeCount*25/60), 'h or', int(totalWatchedEpisodeCount*25/60/24), 'days of watch time.')
