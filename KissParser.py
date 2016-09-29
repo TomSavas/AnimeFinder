9b1a208a00b20a1aa969ab17ee135298f59c1d99
Savas <tomas1savickas@gmail.com>
2016-09-29 14:54:33 +0300

    Fix KissParser.py

--- KissParser.py
+++ KissParser.py
@@ -4,13 +4,14 @@ import SearchThread
 class KissParser():
 	def __init__(self, kissBookmarkUrl=None):
 		self.kissBookmarkUrl = kissBookmarkUrl if kissBookmarkUrl is not None else GetKissBookmarkListUrlFromUser()
+		self.totalEpisodeCount = 0
 		self.searchThreads = []
 		self.animeEntities = []
 
 	def GetBookmarkListOfHtmls(self, kissBookmarkUrl=None):
 		kissBookmarkUrl = self.kissBookmarkUrl if kissBookmarkUrl is None else kissBookmarkUrl
 
-		html = GetHtml(kissBookmarkUrl)
+		html = ScrapeHtml(kissBookmarkUrl)
 		listOfHtmls = regex.split(r'class="trAnime', html)
 		listOfHtmls = list(filter(None, listOfHtmls))
 		return listOfHtmls[1:]
@@ -18,7 +19,7 @@ class KissParser():
 	def GetEntityInfo(self, html, searchThreads=None):
 		searchThreads = self.searchThreads if searchThreads is None else searchThreads
 
-		animeEntity = KissAnimeEntity()
+		animeEntity = AnimeEntity()
 		
 		try:
 			regexResult = regex.search(r'\"aAnime\".href(?:[^\>])+>\s*(?P<AnimeName>[^\<]+)', html)
@@ -40,23 +41,53 @@ class KissParser():
 
 		return animeEntity
 	def GetListOfKissAnimeEntities(self):
+		Debug.Log('[KissParser]Gathering /anime/bookmarklist html')
 		listOfHtmls = self.GetBookmarkListOfHtmls()
 
+		Debug.Log('[KissParser]Parsing to anime entities')
 		for html in listOfHtmls:
 			animeEntity = self.GetEntityInfo(html)
 			if animeEntity is None : continue
 			self.animeEntities.append(animeEntity)
-			
+		Debug.Log('[KissParser]Done parsing to anime entities, amount of entities = ', str(len(self.animeEntities)))
+
+		Debug.Log('[KissParser]Waiting for threads to finish...')	
 		for thread in self.searchThreads:
 			if thread.isAlive():
-				Debug.Log('Gathering data...')
 				time.sleep(5)
 
+		Debug.Log('[KissParser]Gathering episodeCount and synonyms...')			
 		for (animeEntity, thread) in zip(self.animeEntities, self.searchThreads):
 			animeEntity.episodeCount = thread.episodeCount
 			animeEntity.synonyms = thread.synonyms
 
+		Debug.Log('[KissParser]Joining threads...')	
 		for thread in self.searchThreads:
+			self.totalEpisodeCount += thread.episodeCount
 			thread.join()
+		Debug.Log('[KissParser]Finished joining threads...')	
 
-		return self.animeEntities
+		Debug.Log('[KissParser]Syncing with database...')
+		self.SyncWithDatabase()
+
+	def SyncWithDatabase(self):
+		for animeEntity in self.animeEntities:
+			animeEntityFromDB = DB.GetDB().GetKissAnimeEntities(animeName=animeEntity.animeName)
+			if animeEntityFromDB is not None:
+				animeEntityFromDB = animeEntityFromDB[0] 
+				print(animeEntity.__dict__)
+				print(animeEntityFromDB.__dict__)
+				if animeEntity.__dict__ != animeEntityFromDB.__dict__:
+					Debug.Log('[KissParser]Found a missmatch animeName = ', animeEntity.animeName, ' adding to DB...')
+					DB.GetDB().AddKissAnimeEntity(animeEntity)
+			else:
+				Debug.Log('[KissParser]', animeEntity.animeName, ' not found in DB, adding to DB...')
+				DB.GetDB().AddKissAnimeEntity(animeEntity)
+
+	def CountTimeSpent(self):
+		episodeCount = 0
+		for animeEntity in self.animeEntities:
+			if animeEntity.userState.value == 2:
+				episodeCount += animeEntity.episodeCount
+		Debug.Log('You have watched ', episodeCount, ' which @25min rounds to ', str(int(episodeCount*25/60)), ' h or ', str(int(episodeCount*25/60/24)), 'days of watch time.')
+
