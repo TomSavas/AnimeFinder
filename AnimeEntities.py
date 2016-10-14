a973441cde9e0139909aae77aef79d752fba6007
Savas <tomas1savickas@gmail.com>
2016-10-14 11:53:25 +0300

    Add genres and synapse fields

--- AnimeEntities.py
+++ AnimeEntities.py
@@ -2,12 +2,14 @@ from Debug import *
 from UserState import *
 
 class AnimeEntity:
-	def __init__(self, animeName=None, synonyms=None, episodeCount=None, userState=None, animeId=None):
+	def __init__(self, animeName=None, synonyms=None, episodeCount=None, userState=None, animeId=None, genres=None, synapse=None):
 		self.animeName = animeName	
 		self.synonyms = synonyms
 		self.episodeCount = episodeCount
 		self.userState = userState
 		self.animeId = animeId
+		self.genres = genres
+		self.synapse = synapse
 		
 	def Log(self):
 		animeEntityAsString = ''
