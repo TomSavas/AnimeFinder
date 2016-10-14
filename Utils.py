6b8df4bc4d19934864eabf7bce183be4925fe610
Savas <tomas1savickas@gmail.com>
2016-10-14 14:33:08 +0300

    Add CFscrape failure handling in ScrapeHtml function

--- Utils.py
+++ Utils.py
@@ -37,26 +37,6 @@ def ScrapeHtml(url, scraper=None, timesFailed=0):
 		response = ScrapeHtml(url, scraper, timesFailed+1)
 
 	return response
-	# try:
-	# 	if scraper is None:
-	# 		html = str(GetGlobalScraper().get(url).content)
-	# 	else:
-	# 		if crawler:
-	# 			response = scraper.get(url)
-	# 			return response
-	# 		else:
-	# 			html = str(scraper.get(url).content)			
-	# except Exception as exception:
-	# 	if timesFailed > 100:
-	# 		Debug.Log('CFscrape screwed up... \n', traceback.format_exc())
-	# 		return None
-	# 	time.sleep(0.2)
-	# 	if crawler:
-	# 		html = ScrapeHtml(url, scraper, True)
-	# 	html = ScrapeHtml(url, scraper, False)	
-	# 	return html
-	# html = RemoveHtmlTrash(html)
-	# return html
 def RemoveHtmlTrash(html):
 	return html.replace('\r', '').replace('\n', '').replace('\\r', '').replace('\\n', '')
 def GetRequest(url, headers=None):
