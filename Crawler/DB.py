4886a6ab08b47295a59947162826704958e93be3
Savas <tomas1savickas@gmail.com>
2016-11-18 16:56:27 +0200

    Add EpisodeLink support

--- Crawler/DB.py
+++ Crawler/DB.py
@@ -15,7 +15,7 @@ class DB():
 
 		self.cursor.execute('SELECT name FROM sqlite_master WHERE type = "table"')
 		if self.cursor.fetchone() is None:
-			self.cursor.execute("CREATE TABLE KissAnime(AnimeName TEXT PRIMARY KEY, Synonyms TEXT, EpisodeCount INT, UserState TEXT, AnimeID INT, Genres TEXT, Synapse TEXT)")
+			self.cursor.execute("CREATE TABLE KissAnime(AnimeName TEXT PRIMARY KEY, Synonyms TEXT, EpisodeCount INT, UserState TEXT, AnimeID INT, Genres TEXT, Synapse TEXT, Cover TEXT, EpisodePageLinks TEXT, EpisodeLinks TEXT)")
 			Debug.Log('Database created @ ../anime.db')
 		else:
 			Debug.Log('Loaded existing database from ../anime.db')
@@ -27,11 +27,11 @@ class DB():
 	def AddKissAnimeEntities(self, animeEntities):
 		for animeEntity in animeEntities:
 			self.AddKissAnimeEntity(animeEntity)
-	def AddKissAnimeEntity(self, animeEntity):
-		self.cursor.execute('INSERT INTO KissAnime VALUES(?, ?, ?, ?, ?, ?, ?)', self.ParseToTuple(animeEntity))
+	def AddKissAnimeEntity(self, animeEntity, episodePageLinks=None):
+		self.cursor.execute('INSERT INTO KissAnime VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', self.ParseToTuple(animeEntity, episodePageLinks))
 
-	def UpdateKissAnimeEntity(self, animeEntity):
-		self.cursor.execute('UPDATE KissAnime SET EpisodeCount = ?, UserState = ?, AnimeID = ? WHERE AnimeName LIKE ?', self.ParseToTupleForUpdate(animeEntity))
+	def UpdateKissAnimeEntity(self, animeEntity, episodePageLinks):
+		self.cursor.execute('UPDATE KissAnime SET EpisodeCount = ?, UserState = ?, AnimeID = ?, EpisodePageLinks = ? WHERE AnimeName LIKE ?', self.ParseToTupleForUpdate(animeEntity, episodePageLinks))
 
 	def GetKissAnimeEntities(self, animeName=None, animeId=None):
 		animeEntities = []
@@ -52,9 +52,9 @@ class DB():
 		return animeEntities
 
 	def ParseToAnimeEntity(self, values):
-		animeEntity = AnimeEntity(values[0] if values[0] is not None else '', list(filter(lambda x: x != '', values[1].split(';'))) if values[1] is not None else [], values[2] if values[2] is not None else 0, IdentifyUserState(values[3]), values[4], list(filter(lambda x: x != '', values[5].split(';'))) if values[5] is not None else [], values[6])
+		animeEntity = AnimeEntity(values[0] if values[0] is not None else '', list(filter(lambda x: x != '', values[1].split(';'))) if values[1] is not None else [], values[2] if values[2] is not None else 0, IdentifyUserState(values[3]), values[4], list(filter(lambda x: x != '', values[5].split(';'))) if values[5] is not None else [], values[6], values[7], list(filter(lambda x: x != '', values[8].split(';'))) if values[8] is not None else [])
 		return animeEntity
-	def ParseToTuple(self, animeEntity):
+	def ParseToTuple(self, animeEntity, episodePageLinks=None):
 		values = ()
 		values += (animeEntity.animeName if animeEntity.animeName is not None else None, )
 		values += (';'.join(animeEntity.synonyms) if animeEntity.synonyms is not None else None, )
@@ -62,13 +62,17 @@ class DB():
 		values += (animeEntity.userState.name if animeEntity.userState is not None else None, )
 		values += (animeEntity.animeId if animeEntity.animeId is not None else None, )
 		values += (';'.join(animeEntity.genres) if animeEntity.genres is not None else None, )
-		values += (animeEntity.summary if animeEntity.summary is not None else None, )
+		values += (animeEntity.synapse if animeEntity.synapse is not None else None, )
+		values += (animeEntity.cover if animeEntity.cover is not None else None, )
+		values += (';'.join(episodePageLinks if episodePageLinks is not None else []) if animeEntity.episodePageLinks != [] else None, )
+		values += (';'.join(animeEntity.episodes) if animeEntity.episodes is not None else None, )
 		return values
-	def ParseToTupleForUpdate(self, animeEntity):
+	def ParseToTupleForUpdate(self, animeEntity, episodePageLinks=None):
 		values = ()
 		values += (animeEntity.episodeCount if animeEntity.episodeCount is not None else None, )
 		values += (animeEntity.userState.name if animeEntity.userState is not None else None, )
 		values += (animeEntity.animeId if animeEntity.animeId is not None else None, )
+		values += (';'.join(episodePageLinks) if episodePageLinks is not None else None, )
 		values += (animeEntity.animeName if animeEntity.animeName is not None else None, )
 		return values
 
